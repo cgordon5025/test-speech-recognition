12d9db14ac319cc2799eb44e63c21477b02a6a08
unknown <cgordon5025@gmail.com>
2023-06-19 15:24:09 -0400

    figured out mp4 export

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -3,14 +3,43 @@ import React, { useRef, useCallback, useState } from "react";
 
 const Photo = () => {
     const webcamRef = useRef(null)
+    const mediaRecorderRef = useRef(null)
+    const [chunks, setChunks] = useState([])
     const [image, setImage] = useState()
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot()
         setImage(imageSrc)
     }, [webcamRef])
 
-    const recordVideo = () => {
+    const recordVideo = useCallback(() => {
+        mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+            mimeType: 'video/webm'
+        })
+        mediaRecorderRef.current.addEventListener("dataavailable", handleData)
+        mediaRecorderRef.current.start()
+    }, [webcamRef, mediaRecorderRef])
+    const handleData = useCallback(({ data }) => {
+        if (data.size > 0) {
+            setChunks((prev) => prev.concat(data))
+        }
+    })
+    const stopRecording = useCallback(() => {
+        mediaRecorderRef.current.stop()
+    }, [mediaRecorderRef, webcamRef])
 
+    const download = () => {
+        if (chunks.length) {
+            const blob = new Blob(chunks, { type: "video/mp4" });
+            const url = URL.createObjectURL(blob)
+            const a = document.createElement('a')
+            document.body.appendChild(a)
+            a.style = "display:none"
+            a.href = url
+            a.download = "testvideo.mp4"
+            a.click()
+            window.URL.revokeObjectURL(url)
+            setChunks([])
+        }
     }
 
     const videoConstraints = {
@@ -29,6 +58,8 @@ const Photo = () => {
 
             <button onClick={capture}>Capture Photo</button>
             <button onClick={recordVideo}>Start Recording</button>
+            <button onClick={stopRecording}>Stop</button>
+            <button onClick={download}>Download</button>
             <h1>Hello</h1>
             {image ? (
                 <img src={image} alt='screenshot'></img>
